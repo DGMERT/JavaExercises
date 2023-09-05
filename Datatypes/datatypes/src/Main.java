b0541e667c00db33cfc1cb3995308ce775f68749
Sadık <mert06demirtas@hotmail.com>
2023-09-05 10:54:17 +0300

    Temel Veri Tipleri

---
+++ Datatypes/datatypes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //int 4 bytes
+        int i = 15;
+        System.out.println(i);
+
+        //double 8 bytes
+        double e = 19.75;
+        System.out.println(e);
+
+        //boolean 1 bytes
+        boolean dogruMu=false;
+
+    }
+}
