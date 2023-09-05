2afd321edae6feb68d69174efcafef26a5dd4913
Sadık <mert06demirtas@hotmail.com>
2023-09-05 13:41:29 +0300

    for ve while döngüleri

--- Loop Demo/loopDemo/src/Main.java
+++ Loop Demo/loopDemo/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
         for (int i=1;i<10;i++){
             System.out.println(i);
         }
-        System.out.println("Döngü Bitti");
+        System.out.println(" for Döngü Bitti");
+
+        //while
+
+        int i=1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While döngüsü bitti. ");
     }
 }
